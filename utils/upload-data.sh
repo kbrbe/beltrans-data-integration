c03e6b9c21c5837bcbeb6bbe5b48b79ff7f1245f
Sven Lieber <Sven.Lieber@ugent.be>
2021-08-23 11:16:07 +0200

    use environment variables to connect and authenticate for data upload

--- utils/upload-data.sh
+++ utils/upload-data.sh
@@ -11,10 +11,14 @@ then
   exit 1
 fi;
 
+# get environment variables
+export $(cat .env | sed 's/#.*//g' | xargs)
+
 curl -X POST \
+  --user $SPARQL_ENDPOINT_USER_$SPARQL_ENDPOINT_PASSWORD \
   --header "Content-Type: $3" \
   --upload-file $2 \
-http://localhost:8090/bigdata/namespace/$1/sparql
+$SPARQL_ENDPOINT_PUBLIC/namespace/$1/sparql
 
 # if in quad mode a named graph could be specified by attaching the following to the namespace/<ns>/sparql?context-uri=https://my-namespace
 
