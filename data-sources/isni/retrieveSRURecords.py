dca546622ce5dc701436e1e4cd144fdc21b57c0f
Sven Lieber <Sven.Lieber@gmail.com>
2021-11-17 11:04:09 +0000

    reusing an existing resultset and temp api key from a search initiated via a Browser #10

--- data-sources/isni/retrieveSRURecords.py
+++ data-sources/isni/retrieveSRURecords.py
@@ -67,7 +67,12 @@ def main():
 
   namespaces = {'srw': NS_SRW}
 
-  payload = {'operation': 'searchRetrieval', 'version': '1.1', 'recordSchema': 'isni-e', 'maximumRecords': 1000, 'startRecord': 1, 'query': 'pica.noi%3D%22BE%22'}
+  query = "pica.noi%3D%22BE%22"
+  existingQuery = os.getenv('ISNI_RESULT_SET_NAME')
+  authValue = os.getenv('ISNI_API_AUTH_TEMP')
+
+  payload = {'operation': 'searchRetrieve', 'version': '1.1', 'recordSchema': 'isni-e', 'maximumRecords': 500, 'startRecord': 1, 'query': existingQuery, 'x-info-2-auth1.0-authenticationToken': authValue}
+  #payload = {'operation': 'searchRetrieve', 'version': '1.1', 'recordSchema': 'isni-e', 'maximumRecords': 50, 'startRecord': 1, 'query': query}
   url = f'{BASE_URL}/username={USERNAME}/password={PASSWORD}/DB=1.3/'
 
   with open(options.output_file, 'wb') as outFile:
@@ -78,15 +83,19 @@ def main():
     
     #
     # There are currently 60452 records for the search term pica.noi="BE" and the number of max records per request is 1000
-    # Thus loop from 1 to 61000 in steps of 1000 to make 61 requests
+    # We decided to loop from 1 to 61000 in steps of 500 to make several requests
     #
-    for i in range(1, 61000,1000):
+    for i in range(1, 61000,500):
 
       print(f'Iteration {counter}: requesting from {i}')
       payload['startRecord'] = i
 
       try: 
-        r = requests.get(url, params=payload)
+        payloadStr = urllib.parse.urlencode(payload, safe=',+')
+        #print(payloadStr)
+        r = requests.get(url, params=payloadStr)
+        print("requesting: ")
+        print(r.url)
         r.raise_for_status()
       except requests.exceptions.Timeout:
         print(f'There was a timeout in iteration {counter}')
@@ -100,18 +109,25 @@ def main():
       #
       # set the result set ID and authentication token for further requests after obtained from the first request
       #
-      if i == 1:
-        print("First request, obtaining values for further requests")
-        recordSetID = getRecordSetID(r.content)
-        payload['query'] = 'srw.resultSetName%3D' + recordSetID
-        if 'X-SRU-Authentication-Token' in r.headers:
-          payload['x-info-2-auth1.0-authenticationToken'] = r.headers['X-SRU-Authentication-Token']
-          print("values obtained!")
-        else:
-          print("No authentication token found, can't process further requests")
-          exit(1)
+      #if i == 1:
+      #  print("First request, obtaining values for further requests")
+      #  recordSetID = getRecordSetID(r.content)
+      #  print("srw.resultSetName is: '" + str(recordSetID) + "'")
+      #  payload['query'] = 'srw.resultSetName=' + recordSetID
+      #  if 'X-SRU-Authentication-Token' in r.headers:
+      #    print("x-info-2-auth1.0-authenticationToken is: '" + r.headers['X-SRU-Authentication-Token'] + "'")
+      #    payload['x-info-2-auth1.0-authenticationToken'] = r.headers['X-SRU-Authentication-Token']
+      #    print("values obtained!")
+      #    print("payload is now:")
+      #    print(payload)
+      #  else:
+      #    print("No authentication token found, can't process further requests")
+      #    exit(1)
     
       numRecordsWritten = writeRecord(r.content, outFile)
+      if numRecordsWritten == 0:
+        print("No records written, received content was:")
+        print(r.content)
       counter += 1
       print(f'Wrote {numRecordsWritten} result records to the output file, now sleep 5 seconds')
       time.sleep(5)
