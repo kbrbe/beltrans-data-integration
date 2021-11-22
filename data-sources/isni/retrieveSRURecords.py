6e9bbcdfae62dff3ffaad0ac5eed31bb3655403a
Sven Lieber <Sven.Lieber@ugent.be>
2021-11-22 14:50:21 +0100

    fixed API issue and save output in separate files #10 #11
    tested on smaller query with 42 results: pica.noi=BE AND pica.dti <= 1928 AND pica.lni = dut The issue was among others a backslash in the API key which was already URL encoded in the environment file

--- data-sources/isni/retrieveSRURecords.py
+++ data-sources/isni/retrieveSRURecords.py
@@ -8,7 +8,6 @@ import urllib
 from io import BytesIO
 from optparse import OptionParser
 
-NS_SRW = 'http://www.loc.gov/zing/srw/'
 
 # -----------------------------------------------------------------------------
 def getRecordSetID(xmlContent):
@@ -24,7 +23,8 @@ def getRecordSetID(xmlContent):
       return recordSetID
 
 # -----------------------------------------------------------------------------
-def writeRecord(xmlContent, outFile):
+def writeResponseRecords(xmlContent, outFile):
+  """This function writes all found 'responseRecord' tags in xmlContent to file outFile."""
 
   counter = 0
   xmlInputStream = BytesIO(xmlContent)
@@ -44,94 +44,98 @@ def writeRecord(xmlContent, outFile):
 def main():
 
   parser = OptionParser(usage="usage: %prog [options]")
-  parser.add_option('-o', '--output-file', action='store', help='The file in which the cleaned data is stored')
+  parser.add_option('-o', '--output-name-pattern', action='store', help='Files with this name pattern are created in which the downloaded data is stored')
+  parser.add_option('-r', '--number-records', action='store', type='int', help='The number of records requested per API call, max is 1000')
+  parser.add_option('-m', '--max-records', action='store', type='int', help='The maximum number of records which should be requested in total, e.g. 60000 in steps of "--number-records", it cannot be bigger than number-records')
   (options, args) = parser.parse_args()
 
   #
   # Check if we got all required arguments
   #
-  if( (not options.output_file) ):
+  if( (not options.output_name_pattern) or (options.number_records > 1000) or (options.number_records > options.max_records) ):
     parser.print_help()
     exit(1)
 
+  numberRecords = options.number_records
+  maxRecords = options.max_records
+  secondsBetweenAPIRequests = 5
+  baseURL = 'https://isni-m.oclc.org/sru'
+  query = 'pica.noi%3D%22BE%22'
+  dateString = date.today().strftime('%Y-%m-%d')
+  outputFilePrefix = dateString + "-sru-result"
 
   load_dotenv()
 
-  BASE_URL = 'https://isni-m.oclc.org/sru'
   USERNAME = os.getenv('ISNI_SRU_USERNAME')
   PASSWORD = os.getenv('ISNI_SRU_PASSWORD')
-  QUERY = 'pica.noi%3D%22BE%22'
-  MAX_RECORDS = 1000
-  DATE_STRING = date.today().strftime('%Y-%m-%d')
-  OUTPUT_BASENAME = DATE_STRING + "-sru-result"
-
-  namespaces = {'srw': NS_SRW}
-
-  query = "pica.noi%3D%22BE%22"
   existingQuery = os.getenv('ISNI_RESULT_SET_NAME')
   authValue = os.getenv('ISNI_API_AUTH_TEMP')
 
-  payload = {'operation': 'searchRetrieve', 'version': '1.1', 'recordSchema': 'isni-e', 'maximumRecords': 500, 'startRecord': 1, 'query': existingQuery, 'x-info-2-auth1.0-authenticationToken': authValue}
+  payload = {'operation': 'searchRetrieve', 'version': '1.1', 'startRecord': 1, 'maximumRecords': numberRecords, 'recordSchema': 'isni-e', 'sortKeys': 'none', 'query': existingQuery, 'x-info-2-auth1.0-authenticationToken': authValue}
   #payload = {'operation': 'searchRetrieve', 'version': '1.1', 'recordSchema': 'isni-e', 'maximumRecords': 50, 'startRecord': 1, 'query': query}
-  url = f'{BASE_URL}/username={USERNAME}/password={PASSWORD}/DB=1.3/'
+  url = f'{baseURL}/username={USERNAME}/password={PASSWORD}/DB=1.3'
 
-  with open(options.output_file, 'wb') as outFile:
-
-    outFile.write(b'<collection>')
-    recordSetID = None
-    counter = 1
+  recordSetID = None
+  counter = 1
     
+  #
+  # There are currently 60452 records for the search term pica.noi="BE" and the number of max records per request is 1000
+  # We decided to loop from 1 to 61000 in steps of 500 to make several requests
+  #
+  for i in range(1, maxRecords, numberRecords):
+
+    print(f'Iteration {counter}: requesting {numberRecords} from startRecord {i} (up until {maxRecords})')
+    payload['startRecord'] = i
+
+    try: 
+      payloadStr = urllib.parse.urlencode(payload, safe=',+*\\')
+      #print(payloadStr)
+      r = requests.get(url, params=payloadStr)
+      print("requesting: ")
+      print(r.url)
+      r.raise_for_status()
+    except requests.exceptions.Timeout:
+      print(f'There was a timeout in iteration {counter}')
+    except requests.exceptions.TooManyRedirects:
+      print(f'There were too many redirects in iteration {counter}')
+    except requests.exceptions.HTTPError as err:
+      print(f'There was an HTTP response code which is not 200 in iteration {counter}')
+    except requests.exceptions.RequestException as e:
+      raise SystemExit(e)
+
     #
-    # There are currently 60452 records for the search term pica.noi="BE" and the number of max records per request is 1000
-    # We decided to loop from 1 to 61000 in steps of 500 to make several requests
+    # set the result set ID and authentication token for further requests after obtained from the first request
     #
-    for i in range(1, 61000,500):
-
-      print(f'Iteration {counter}: requesting from {i}')
-      payload['startRecord'] = i
-
-      try: 
-        payloadStr = urllib.parse.urlencode(payload, safe=',+')
-        #print(payloadStr)
-        r = requests.get(url, params=payloadStr)
-        print("requesting: ")
-        print(r.url)
-        r.raise_for_status()
-      except requests.exceptions.Timeout:
-        print(f'There was a timeout in iteration {counter}')
-      except requests.exceptions.TooManyRedirects:
-        print(f'There were too many redirects in iteration {counter}')
-      except requests.exceptions.HTTPError as err:
-        print(f'There was an HTTP response code which is not 200 in iteration {counter}')
-      except requests.exceptions.RequestException as e:
-        raise SystemExit(e)
-
-      #
-      # set the result set ID and authentication token for further requests after obtained from the first request
-      #
-      #if i == 1:
-      #  print("First request, obtaining values for further requests")
-      #  recordSetID = getRecordSetID(r.content)
-      #  print("srw.resultSetName is: '" + str(recordSetID) + "'")
-      #  payload['query'] = 'srw.resultSetName=' + recordSetID
-      #  if 'X-SRU-Authentication-Token' in r.headers:
-      #    print("x-info-2-auth1.0-authenticationToken is: '" + r.headers['X-SRU-Authentication-Token'] + "'")
-      #    payload['x-info-2-auth1.0-authenticationToken'] = r.headers['X-SRU-Authentication-Token']
-      #    print("values obtained!")
-      #    print("payload is now:")
-      #    print(payload)
-      #  else:
-      #    print("No authentication token found, can't process further requests")
-      #    exit(1)
+    #if i == 1:
+    #  print("First request, obtaining values for further requests")
+    #  recordSetID = getRecordSetID(r.content)
+    #  print("srw.resultSetName is: '" + str(recordSetID) + "'")
+    #  payload['query'] = 'srw.resultSetName=' + recordSetID
+    #  if 'X-SRU-Authentication-Token' in r.headers:
+    #    print("x-info-2-auth1.0-authenticationToken is: '" + r.headers['X-SRU-Authentication-Token'] + "'")
+    #    payload['x-info-2-auth1.0-authenticationToken'] = r.headers['X-SRU-Authentication-Token']
+    #    print("values obtained!")
+    #    print("payload is now:")
+    #    print(payload)
+    #  else:
+    #    print("No authentication token found, can't process further requests")
+    #    exit(1)
     
-      numRecordsWritten = writeRecord(r.content, outFile)
+
+    #
+    # create a new file for the current paginated output
+    # 
+    outputFilename = f'{outputFilePrefix}-{options.output_name_pattern}-{i}-{numberRecords}.xml'
+    with open(outputFilename, 'wb') as outFile:
+      outFile.write(b'<collection>')
+      numRecordsWritten = writeResponseRecords(r.content, outFile)
+      outFile.write(b'</collection>')
       if numRecordsWritten == 0:
         print("No records written, received content was:")
         print(r.content)
       counter += 1
-      print(f'Wrote {numRecordsWritten} result records to the output file, now sleep 5 seconds')
-      time.sleep(5)
+      print(f'Wrote {numRecordsWritten} result records to the output file {outputFilename}, now sleep {secondsBetweenAPIRequests} seconds')
+    time.sleep(secondsBetweenAPIRequests)
 
-    outFile.write(b'</collection>')
 
 main()
