9045a507572396d7c61fa82cff2be4d71d81d050
Sven Lieber <Sven.Lieber@gmail.com>
2021-11-24 12:10:39 +0000

    added function needed in some scripts

--- data-sources/isni/utils.py
+++ data-sources/isni/utils.py
@@ -21,13 +21,13 @@ def countStat(stats, counter, value):
   >>> stats = {}
   >>> countStat(stats, 'myCounter', 1)
   >>> stats['myCounter']
-  {'min': 1, 'max': 1, 'avg': 1, 'number': 1, 'values': [1]}
+  {'min': 1.0, 'max': 1.0, 'avg': 1.0, 'number': 1, 'values': [1.0]}
 
   Stats are correctly computed and added for already existing counters.
-  >>> stats =  { 'myCounter': {'min': 1, 'max': 3, 'avg': 2, 'number': 3, 'values': [1, 2, 3]}}
+  >>> stats =  { 'myCounter': {'min': 1.0, 'max': 3.0, 'avg': 2, 'number': 3, 'values': [1.0, 2.0, 3.0]}}
   >>> countStat(stats, 'myCounter', 7)
   >>> stats['myCounter']
-  {'min': 1, 'max': 7, 'avg': 3.25, 'number': 4, 'values': [1, 2, 3, 7]}
+  {'min': 1.0, 'max': 7.0, 'avg': 3.25, 'number': 4, 'values': [1.0, 2.0, 3.0, 7.0]}
   """
   value = float(value)
   if counter in stats:
@@ -37,8 +37,44 @@ def countStat(stats, counter, value):
     stats[counter]['max'] = max(stats[counter]['max'], value)
     stats[counter]['avg'] = sum(stats[counter]['values'])/len(stats[counter]['values'])
   else:
-    stats[counter] = {'min': value, 'max': value, 'avg': value, 'number': value, 'values': [value]}
+    stats[counter] = {'min': value, 'max': value, 'avg': value, 'number': 1, 'values': [value]}
 
+
+# -----------------------------------------------------------------------------
+def getElementValue(elem, sep=';'):
+  """This function returns the value of the element if it is not None, otherwise an empty string.
+
+  The function returns the 'text' value if there is one
+  >>> class Test: text = 'hello'
+  >>> obj = Test()
+  >>> getElementValue(obj)
+  'hello'
+
+  It returns nothing if there is no text value
+  >>> class Test: pass
+  >>> obj = Test()
+  >>> getElementValue(obj)
+  ''
+
+  And the function returns a semicolon separated list in case the argument is a list of objects with a 'text' attribute
+  >>> class Test: text = 'hello'
+  >>> obj1 = Test()
+  >>> obj2 = Test()
+  >>> getElementValue([obj1,obj2])
+  'hello;hello'
+  """
+  if elem is not None:
+    if isinstance(elem, list):
+      valueList = list()
+      for e in elem:
+        if hasattr(e, 'text'):
+          valueList.append(e.text)
+      return ';'.join(valueList)
+    else:
+      if hasattr(elem, 'text'):
+        return elem.text
+  
+  return ''
 
 
 # -----------------------------------------------------------------------------
