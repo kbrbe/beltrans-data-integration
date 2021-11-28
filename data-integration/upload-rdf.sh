9f9122b57683ef2c7740802d244ecc9b4c5134de
Sven Lieber <Sven.Lieber@ugent.be>
2021-11-28 23:53:11 +0100

    updated scripts for data upload to fix #15
    uploading data to named graphs of one Blazegraph database. Using several databases we have to use also federated querying which gives authentication errors although we specify username and password as part of the SERVICE definition

---
+++ data-integration/upload-rdf.sh
@@ -0,0 +1,4 @@
+bash upload-data.sh integration ../data-sources/translations.ttl "application/x-turtle" http://wikibase-test-srv01.kbr.be/sparql http://kbr-syracuse
+bash upload-data.sh integration ../data-sources/isni/authorities-sru.ttl "application/x-turtle" http://wikibase-test-srv01.kbr.be/sparql http://isni-sru
+bash upload-data.sh integration ../data-sources/isni/rdf/2021-11-28-belgians-isni.rdf "application/rdf+xml" http://wikibase-test-srv01.kbr.be/sparql http://isni-rdf
+bash upload-data.sh integration ../data-sources/kbr/kbr-authors.ttl "application/x-turtle" http://wikibase-test-srv01.kbr.be/sparql http://kbr-belgians
---
+++ utils/delete-named-graph.sh
@@ -0,0 +1,16 @@
+
+if [ $# -ne 3 ];
+then
+  echo "Please provide a namespace, the SPARQL endpoint you would like to query and the name of the named graph you would like to delete."
+  exit 1
+fi;
+
+
+# get environment variables
+export $(cat .env | sed 's/#.*//g' | xargs)
+
+curl -D- -X DELETE \
+  --user $SPARQL_ENDPOINT_USER:$SPARQL_ENDPOINT_PASSWORD \"
+"$2/namespace/$1/sparql?c=<$3>"
+
+
