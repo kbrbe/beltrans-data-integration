5961ecb5d7fc52364b7eec28342fcae2d3ae47c7
Sven Lieber <Sven.Lieber@ugent.be>
2021-12-03 12:19:05 +0100

    added script to convert thesauri to a more machine-friendly format such that we can map it to RDF #33

---
+++ data-sources/master-data/convert-thesaurus.py
@@ -0,0 +1,82 @@
+#
+# (c) 2021 Sven Lieber
+# KBR Brussels
+#
+import csv
+from optparse import OptionParser
+
+
+# -----------------------------------------------------------------------------
+def writeResult(writer, rowID, name, parent):
+  writer.writerow([rowID, name.strip(), parent])
+
+def main():
+  """This script converts a thesaurus CSV where each level is specified in another column to a more machine-friendly format."""
+
+  parser = OptionParser(usage="usage: %prog [options]")
+  parser.add_option('-i', '--input-file', action='store', help='The input file containing CSV data')
+  parser.add_option('-o', '--output-file', action='store', help='The file in which content with new headers is stored')
+  (options, args) = parser.parse_args()
+
+  #
+  # Check if we got all required arguments
+  #
+  if( (not options.input_file) or (not options.output_file) ):
+    parser.print_help()
+    exit(1)
+
+  with open(options.input_file, 'r', encoding="latin_1") as inFile, \
+       open(options.output_file, 'w', encoding="latin_1") as outFile:
+
+    inputReader = csv.DictReader(inFile, delimiter=';')
+    outputWriter = csv.writer(outFile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    outputWriter.writerow(['id', 'name', 'parentID'])
+    parents = list()
+    lastLevel = 1
+    for row in inputReader:
+      # the first 10 columns refer to the different thesaurus levels, DictReader gives us the column names in the original order
+      # iterate over these first 10 columns determine the value and parent
+      numNameColumns = 10
+      currentCol = 1
+      rowID = row['Identifiant']
+      for (col, val) in row.items():
+        if(currentCol == numNameColumns):
+          break
+        if val != '':
+          currentLevel = currentCol
+          if len(parents) == 0:
+            writeResult(outputWriter, rowID, val, '')
+            parents.append(rowID)
+            lastLevel = currentLevel
+          else:
+            if currentLevel > lastLevel:
+              writeResult(outputWriter, rowID, val, parents[-1])
+              parents.append(rowID)
+              lastLevel = currentLevel
+            elif currentLevel == lastLevel:
+              parents.pop()
+              writeResult(outputWriter, rowID, val, parents[-1])
+              parents.append(rowID)
+              lastLevel = currentLevel
+            else:
+              # it is smaller, so now entries of a previous hierarchy
+              # remove the current element because it is not a parent
+              parents.pop()
+              # and also remove other parents, e.g. we were in level 4, but now we are in a hierarchy for 1, thus remove 4-1 = 3
+              levelDiff = lastLevel - currentLevel
+              for i in range(levelDiff):
+                parents.pop()
+              lastLevel = currentLevel
+              if len(parents) > 0:
+                writeResult(outputWriter, rowID, val, parents[-1])
+              else:
+                # we are again at a new root
+                writeResult(outputWriter, rowID, val, '')
+              parents.append(rowID)
+              
+         
+        currentCol += 1
+
+
+main()
