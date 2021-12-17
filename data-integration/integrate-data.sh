e187a29e936a3eaa2c1e683551ec123ac361043f
Sven Lieber <Sven.Lieber@ugent.be>
2021-12-17 14:30:31 +0100

    also extract and transform linked authorities #46

--- data-integration/integrate-data.sh
+++ data-integration/integrate-data.sh
@@ -6,13 +6,24 @@ SCRIPT_TRANSFORM_TRANSLATIONS="../data-sources/kbr/marc-to-csv.py"
 SCRIPT_NORMALIZE_HEADERS="../data-sources/kbr/replace-headers.py"
 SCRIPT_EXTRACT_IDENTIFIED_AUTHORITIES="../data-sources/kbr/get-identified-authorities.sh"
 
+KBR_CSV_HEADER_CONVERSION="../data-sources/kbr/author-headers.csv"
+
 #
 # Input filenames
 #
-# KBR
+# KBR - translations
 INPUT_KBR_TRL_NL="../data-sources/kbr/translations/ExportSyracuse_20211213_NL-FR_1970-2020_3866records.xml"
 INPUT_KBR_TRL_FR="../data-sources/kbr/translations/ExportSyracuse_20211213_FR-NL_1970-2020_9239records.xml"
 
+# KBR - linked authorities
+INPUT_KBR_LA_PERSON_NL="../data-sources/kbr/agents/KBR_1970-2020_NL-FR_AUT-lies_APEP_3769records-all-fields.csv"
+INPUT_KBR_LA_ORG_NL="../data-sources/kbr/agents/KBR_1970-2020_NL-FR_AUT-lies_AORG_711records-all-fields.csv"
+INPUT_KBR_LA_PERSON_FR="../data-sources/kbr/agents/KBR_1970-2020_FR-NL_AUT-lies_APEP_8384records-all-fields.csv"
+INPUT_KBR_LA_ORG_FR="../data-sources/kbr/agents/KBR_1970-2020_FR-NL_AUT-lies_AORG_702records-all-fields.csv"
+
+INPUT_KBR_LA_PLACES_VLG="../data-sources/kbr/agents/publisher-places-VLG.csv"
+INPUT_KBR_LA_PLACES_WAL="../data-sources/kbr/agents/publisher-places-WAL.csv"
+INPUT_KBR_LA_PLACES_BRU="../data-sources/kbr/agents/publisher-places-BRU.csv"
 
 #
 # Filenames used within an integration directory 
@@ -30,10 +41,21 @@ SUFFIX_KBR_TRL_FR_WORKS="fr-translations-works.csv"
 SUFFIX_KBR_TRL_FR_CONT="fr-translations-contributors.csv"
 SUFFIX_KBR_TRL_FR_NEWAUT="fr-translations-identified-authorities.csv"
 
+SUFFIX_KBR_LA_PLACES_VLG="publisher-places-VLG.csv"
+SUFFIX_KBR_LA_PLACES_WAL="publisher-places-WAL.csv"
+SUFFIX_KBR_LA_PLACES_BRU="publisher-places-BRU.csv"
+
 # DATA SOURCE - KBR LINKED AUTHORITIES
 #
-SUFFIX_KBR_LA_NL="nl-translations-identified-authorities.csv"
-SUFFIX_KBR_LA_FR="fr-translations-identified-authorities.csv"
+SUFFIX_KBR_LA_PERSONS_NL_CLEANED="nl-translations-linked-authorities-persons-cleaned.csv"
+SUFFIX_KBR_LA_ORGS_NL_CLEANED="nl-translations-linked-authorities-orgs-cleaned.csv"
+SUFFIX_KBR_LA_PERSONS_FR_CLEANED="fr-translations-linked-authorities-persons-cleaned.csv"
+SUFFIX_KBR_LA_ORGS_FR_CLEANED="fr-translations-linked-authorities-orgs-cleaned.csv"
+
+SUFFIX_KBR_LA_PERSONS_NL_NORM="nl-translations-linked-authorities-persons-norm.csv"
+SUFFIX_KBR_LA_ORGS_NL_NORM="nl-translations-linked-authorities-orgs-norm.csv"
+SUFFIX_KBR_LA_PERSONS_FR_NORM="fr-translations-linked-authorities-persons-norm.csv"
+SUFFIX_KBR_LA_ORGS_FR_NORM="fr-translations-linked-authorities-orgs-norm.csv"
 
 #
 # LINKED DATA - KBR TRANSLATIONS
@@ -117,8 +139,11 @@ function extractKBR {
   mkdir -p $integrationName/kbr/translations
   mkdir -p $integrationName/kbr/agents
 
-  echo "EXTRACTION - Extract and clean KBR data"
+  echo "EXTRACTION - Extract and clean KBR translations data"
   extractKBRTranslationsAndContributions "$integrationName" "$INPUT_KBR_TRL_NL" "$INPUT_KBR_TRL_FR"
+
+  echo "EXTRACTION - Extract and clean KBR linked authorities data"
+  extractKBRLinkedAuthorities "$integrationName" "$INPUT_KBR_LA_PERSON_NL" "$INPUT_KBR_LA_ORG_NL" "$INPUT_KBR_LA_PERSON_FR" "$INPUT_KBR_LA_ORG_FR"
 
 }
 
@@ -130,17 +155,11 @@ function transformKBR {
   # create the folder to place the transformed data
   mkdir -p $integrationName/kbr/rdf 
 
-  echo "TRANSFORMATION - Map KBR data to RDF"
+  echo "TRANSFORMATION - Map KBR translation data to RDF"
   mapKBRTranslationsAndContributions $integrationName
-}
 
-# -----------------------------------------------------------------------------
-function mapKBRLinkedAuthorities {
-
-  kbrDutchLinkedOrgs="../data-sources/kbr/agents/KBR_1970-2020_NL-FR_AUT-lies_AORG_xxrecords-all-fields.csv"
-  kbrDutchLinkedPersons="../data-sources/kbr/agents/KBR_1970-2020_NL-FR_AUT-lies_APEP_xxrecords-all-fields.csv"
-  kbrFrenchLinkedOrgs="../data-sources/kbr/agents/KBR_1970-2020_FR-NL_AUT-lies_AORG_xxrecords-all-fields.csv"
-  kbrFrenchLinkedPersons="../data-sources/kbr/agents/KBR_1970-2020_FR-NL_AUT-lies_APEP_xxrecords-all-fields.csv"
+  echo "TRANSFORMATION - Map KBR linked authorities data to RDF"
+  mapKBRLinkedAuthorities $integrationName
 }
 
 # -----------------------------------------------------------------------------
@@ -158,7 +177,7 @@ function extractKBRTranslationsAndContributions {
 
 
   # document which input was used
-  printf "Used input\n* $kbrDutchTranslations\n* $kbrFrenchTranslations" > "$integrationName/kbr/README.md"
+  printf "Used input\n* $kbrDutchTranslations\n* $kbrFrenchTranslations" >> "$integrationName/kbr/README.md"
 
   #
   # Define file names based on current integration directory and file name patterns
@@ -193,6 +212,56 @@ function extractKBRTranslationsAndContributions {
 }
 
 # -----------------------------------------------------------------------------
+function extractKBRLinkedAuthorities {
+
+  local integrationName=$1
+  local kbrNLPersons=$2
+  local kbrNLOrgs=$3
+  local kbrFRPersons=$4
+  local kbrFROrgs=$5
+
+  # document which input was used
+  printf "Used input\n* $kbrNLPersons\n* $kbrNLOrgs\n* $kbrFRPersons\n*$kbrFROrgs" >> "$integrationName/kbr/README.md"
+
+  #
+  # Define file names based on current integration directory and file name patterns
+  #
+  kbrNLPersonsNorm="$integrationName/kbr/agents/$SUFFIX_KBR_LA_PERSONS_NL_NORM"
+  kbrNLOrgsNorm="$integrationName/kbr/agents/$SUFFIX_KBR_LA_ORGS_NL_NORM"
+  kbrFRPersonsNorm="$integrationName/kbr/agents/$SUFFIX_KBR_LA_PERSONS_FR_NORM"
+  kbrFROrgsNorm="$integrationName/kbr/agents/$SUFFIX_KBR_LA_ORGS_FR_NORM"
+
+  kbrNLPersonsCleaned="$integrationName/kbr/agents/$SUFFIX_KBR_LA_PERSONS_NL_CLEANED"
+  kbrNLOrgsCleaned="$integrationName/kbr/agents/$SUFFIX_KBR_LA_ORGS_NL_CLEANED"
+  kbrFRPersonsCleaned="$integrationName/kbr/agents/$SUFFIX_KBR_LA_PERSONS_FR_CLEANED"
+  kbrFROrgsCleaned="$integrationName/kbr/agents/$SUFFIX_KBR_LA_ORGS_FR_CLEANED"
+
+  source ../data-sources/py-etl-env/bin/activate
+
+  echo "Clean authorities NL - Persons ..."
+  normalizeCSVHeaders "$kbrNLPersons" "$kbrNLPersonsNorm" "$KBR_CSV_HEADER_CONVERSION"
+  cleanAgents "$kbrNLPersonsNorm" "$kbrNLPersonsCleaned"
+
+  echo "Clean authorities NL - Organizations ..."
+  normalizeCSVHeaders "$kbrNLOrgs" "$kbrNLOrgsNorm" "$KBR_CSV_HEADER_CONVERSION"
+  cleanAgents "$kbrNLOrgsNorm" "$kbrNLOrgsCleaned"
+
+  echo "Clean authorities FR - Persons ..."
+  normalizeCSVHeaders "$kbrFRPersons" "$kbrFRPersonsNorm" "$KBR_CSV_HEADER_CONVERSION"
+  cleanAgents "$kbrFRPersonsNorm" "$kbrFRPersonsCleaned"
+
+  echo "Clean authorities FR - Organizations ..."
+  normalizeCSVHeaders "$kbrFROrgs" "$kbrFROrgsNorm" "$KBR_CSV_HEADER_CONVERSION"
+  cleanAgents "$kbrFROrgsNorm" "$kbrFROrgsCleaned"
+
+  echo "Copy publisher location information ..."
+  cp "$INPUT_KBR_LA_PLACES_VLG" "$integrationName/kbr/agents/$SUFFIX_KBR_LA_PLACES_VLG"
+  cp "$INPUT_KBR_LA_PLACES_WAL" "$integrationName/kbr/agents/$SUFFIX_KBR_LA_PLACES_WAL"
+  cp "$INPUT_KBR_LA_PLACES_BRU" "$integrationName/kbr/agents/$SUFFIX_KBR_LA_PLACES_BRU"
+ 
+}
+
+# -----------------------------------------------------------------------------
 function mapKBRTranslationsAndContributions {
 
   local integrationName=$1
@@ -209,7 +278,7 @@ function mapKBRTranslationsAndContributions {
   export RML_SOURCE_CONT_NL="$integrationName/kbr/translations/$SUFFIX_KBR_TRL_NL_CONT"
 
   # 2) execute the mapping
-  echo "Map translations and contributions ..."
+  echo "Map KBR translations and contributions ..."
   . map.sh ../data-sources/kbr/kbr-translations.yml $kbrTranslationsTurtle
 
 
@@ -220,11 +289,38 @@ function mapKBRTranslationsAndContributions {
   export RML_SOURCE_KBR_CONT_FR_IDENTIFIED="$integrationName/kbr/translations/$SUFFIX_KBR_TRL_FR_NEWAUT"
 
   # 2) execute the mapping
-  echo "Map newly identified contributors ..."
+  echo "Map KBR newly identified contributors ..."
   . map.sh ../data-sources/kbr/kbr-identified-authorities.yml $kbrTranslationsIdentifiedAuthorities
 
 
 }
+
+# -----------------------------------------------------------------------------
+function mapKBRLinkedAuthorities {
+
+  local integrationName=$1
+
+  kbrLinkedAuthoritiesTurtle="$integrationName/kbr/rdf/$SUFFIX_KBR_LA_LD"
+
+  # map the linked authorities
+
+  # 1) specify the input for the mapping (env variables taken into account by the YARRRML mapping)
+  export RML_SOURCE_KBR_LINKED_AUTHORITIES_PERSONS_NL="$integrationName/kbr/agents/$SUFFIX_KBR_LA_PERSONS_NL_CLEANED"
+  export RML_SOURCE_KBR_LINKED_AUTHORITIES_PERSONS_FR="$integrationName/kbr/agents/$SUFFIX_KBR_LA_PERSONS_FR_CLEANED"
+  export RML_SOURCE_KBR_LINKED_AUTHORITIES_ORGS_NL="$integrationName/kbr/agents/$SUFFIX_KBR_LA_ORGS_NL_CLEANED"
+  export RML_SOURCE_KBR_LINKED_AUTHORITIES_ORGS_FR="$integrationName/kbr/agents/$SUFFIX_KBR_LA_ORGS_FR_CLEANED"
+
+  export RML_SOURCE_KBR_PUBLISHER_PLACES_FLANDERS="$integrationName/kbr/agents/$SUFFIX_KBR_LA_PLACES_VLG"
+  export RML_SOURCE_KBR_PUBLISHER_PLACES_WALLONIA="$integrationName/kbr/agents/$SUFFIX_KBR_LA_PLACES_WAL"
+  export RML_SOURCE_KBR_PUBLISHER_PLACES_BRUSSELS="$integrationName/kbr/agents/$SUFFIX_KBR_LA_PLACES_BRU"
+
+  # 2) execute the mapping
+  echo "Map KBR linked authorities ..."
+  . map.sh ../data-sources/kbr/kbr-linked-authorities.yml $kbrLinkedAuthoritiesTurtle
+
+}
+
+
 
 # -----------------------------------------------------------------------------
 function checkFile {
