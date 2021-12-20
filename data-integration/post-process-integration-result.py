382f410c309f953ebe2cbc6f66effbf7df2f1870
Sven Lieber <Sven.Lieber@ugent.be>
2021-12-20 12:50:33 +0100

    first draft of selecting only one birth/death date (todo: select based on most complete) #41

--- data-integration/post-process-integration-result.py
+++ data-integration/post-process-integration-result.py
@@ -4,9 +4,10 @@
 #
 import csv
 from optparse import OptionParser
+import utils
 
 
-
+# -----------------------------------------------------------------------------
 def main():
   """This script filters the SPARQL query result of the integrated data."""
 
@@ -29,20 +30,49 @@ def main():
        open(options.output_file, 'w', encoding="utf-8") as outFile:
 
     inputReader = csv.DictReader(inFile, delimiter=',')
-    headers = inputReader.fieldnames
+    headers = inputReader.fieldnames.copy()
+
+    # In the output we only want a single birth date and a single death date column
+    # thus first remove the respective columns from different data sources
+    headersToRemove = ['authorBirthDateKBR', 'authorDeathDateKBR', 'authorBirthDateISNI', 'authorDeathDateISNI',
+                       'translatorBirthDateKBR', 'translatorDeathDateKBR', 'translatorBirthDateISNI', 'translatorDeathDateISNI',
+                       'illustratorBirthDateKBR', 'illustratorDeathDateKBR', 'illustratorBirthDateISNI', 'illustratorDeathDateISNI',
+                       'scenaristBirthDateKBR', 'scenaristDeathDateKBR', 'scenaristBirthDateISNI', 'scenaristDeathDateISNI']
+    for r in headersToRemove:
+      headers.remove(r)
+
+    # and then add the single columns we want
+    headers.extend(['authorBirthDate', 'authorDeathDate', 
+                   'translatorBirthDate', 'translatorDeathDate',
+                   'illustratorBirthDate', 'illustratorDeathDate',
+                   'scenaristBirthDate', 'scenaristDeathDate'])
+
     outputWriter = csv.DictWriter(outFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL, fieldnames=headers)
 
     outputWriter.writeheader()
 
     belgian = 'Belgium'
 
+    # used to parse certain columns based on their name, e.g. authorBirthDateKBR and authorBirthDateISNI
+    sources = ['KBR', 'ISNI']
+
     # write relevant data to output
     for row in inputReader:
       if( row['authorNationality'] == belgian or row['illustratorNationality'] == belgian or row['scenaristNationality'] == belgian):
+
+        # This is a relevant row, we process it further before writing it
+        utils.selectDate(row, 'author', 'Birth', sources)
+        utils.selectDate(row, 'illustrator', 'Birth', sources)
+        utils.selectDate(row, 'translator', 'Birth', sources)
+        utils.selectDate(row, 'scenarist', 'Birth', sources)
+
+        utils.selectDate(row, 'author', 'Death', sources)
+        utils.selectDate(row, 'illustrator', 'Death', sources)
+        utils.selectDate(row, 'translator', 'Death', sources)
+        utils.selectDate(row, 'scenarist', 'Death', sources)
+
         outputWriter.writerow(row)
-      #if( row['authorNationality'] == belgian or row['illustratorNationality'] == belgian or row['scenaristNationality'] == belgian
-      #    or row['authorNationality'] == '' or row['illustratorNationality'] == '' or row['scenaristNationality'] == ''):
-      #  outputWriter.writerow(row)
+
 
 
 main()
