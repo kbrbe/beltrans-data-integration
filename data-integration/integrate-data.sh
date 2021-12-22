848dcdafdbdb0fe33e3b2dbf39157a9f7665c276
Sven Lieber <Sven.Lieber@ugent.be>
2021-12-22 16:10:48 +0100

    create statistics for translations per country and language and per location and language to fix #52

--- data-integration/integrate-data.sh
+++ data-integration/integrate-data.sh
@@ -72,11 +72,15 @@ DATA_PROFILE_QUERY_FILE="dataprofile.sparql"
 DATA_PROFILE_AGG_QUERY_FILE="dataprofile-aggregated.sparql"
 DATA_PROFILE_CONT_QUERY_FILE="contributors.sparql"
 DATA_PROFILE_PUBS_PER_YEAR_QUERY_FILE="translations-per-year.sparql"
+DATA_PROFILE_PUBS_PER_LOC_QUERY_FILE="translations-per-location.sparql"
+DATA_PROFILE_PUBS_PER_COUNTRY_QUERY_FILE="translations-per-country.sparql"
 
 SUFFIX_DATA_PROFILE_FILE="integrated-data.csv"
 SUFFIX_DATA_PROFILE_CONT_FILE="integrated-data-contributors.csv"
 SUFFIX_DATA_PROFILE_AGG_FILE="integrated-data-aggregated.csv"
 SUFFIX_DATA_PROFILE_PUBS_PER_YEAR_FILE="translations-per-year.csv"
+SUFFIX_DATA_PROFILE_PUBS_PER_LOC_FILE="translations-per-location.csv"
+SUFFIX_DATA_PROFILE_PUBS_PER_COUNTRY_FILE="translations-per-country.csv"
 
 SUFFIX_DATA_PROFILE_FILE_PROCESSED="integrated-data-processed.csv"
 SUFFIX_DATA_PROFILE_CONT_FILE_PROCESSED="integrated-data-contributors-processed.csv"
@@ -240,11 +244,15 @@ function query {
   queryFileAgg="$DATA_PROFILE_AGG_QUERY_FILE"
   queryFileCont="$DATA_PROFILE_CONT_QUERY_FILE"
   queryFilePubsPerYear="$DATA_PROFILE_PUBS_PER_YEAR_QUERY_FILE"
+  queryFilePubsPerCountry="$DATA_PROFILE_PUBS_PER_COUNTRY_QUERY_FILE"
+  queryFilePubsPerLoc="$DATA_PROFILE_PUBS_PER_LOC_QUERY_FILE"
 
   outputFile="$integrationName/$SUFFIX_DATA_PROFILE_FILE"
   outputFileAgg="$integrationName/$SUFFIX_DATA_PROFILE_AGG_FILE"
   outputFileCont="$integrationName/$SUFFIX_DATA_PROFILE_CONT_FILE"
   outputFilePubsPerYear="$integrationName/$SUFFIX_DATA_PROFILE_PUBS_PER_YEAR_FILE"
+  outputFilePubsPerCountry="$integrationName/$SUFFIX_DATA_PROFILE_PUBS_PER_COUNTRY_FILE"
+  outputFilePubsPerLoc="$integrationName/$SUFFIX_DATA_PROFILE_PUBS_PER_LOC_FILE"
 
   echo "Creating the dataprofile CSV file ..."
   queryData "$TRIPLE_STORE_NAMESPACE" "$queryFile" "$ENV_SPARQL_ENDPOINT" "$outputFile"
@@ -257,6 +265,12 @@ function query {
 
   echo "Creating statistics about publications per language and year ..."
   queryData "$TRIPLE_STORE_NAMESPACE" "$queryFilePubsPerYear" "$ENV_SPARQL_ENDPOINT" "$outputFilePubsPerYear"
+
+  echo "Creating statistics about publications per language and country ..."
+  queryData "$TRIPLE_STORE_NAMESPACE" "$queryFilePubsPerCountry" "$ENV_SPARQL_ENDPOINT" "$outputFilePubsPerCountry"
+
+  echo "Creating statistics about publications per language and location ..."
+  queryData "$TRIPLE_STORE_NAMESPACE" "$queryFilePubsPerLoc" "$ENV_SPARQL_ENDPOINT" "$outputFilePubsPerLoc"
 }
 
 # -----------------------------------------------------------------------------
