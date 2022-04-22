176f9aeac0d7296b6c8c89cfa85c24cf01d3d211
Sven Lieber <Sven.Lieber@ugent.be>
2022-04-22 15:53:13 +0200

    added new integration option

--- data-integration/integrate-data.sh
+++ data-integration/integrate-data.sh
@@ -1739,6 +1739,13 @@ else
     transform $2 $3
     load $2 $3
 
+  elif [ "$1" = "etli" ];
+  then
+    extract $2 $3
+    transform $2 $3
+    load $2 $3
+    integrate $3
+
   elif [ "$1" = "e" ];
   then
     extract $2 $3
