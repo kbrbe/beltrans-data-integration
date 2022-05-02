37adff156b2bada7f5b09fa991e3c782b90a170d
Sven Lieber <Sven.Lieber@ugent.be>
2022-05-02 16:50:24 +0200

    added unit tests for country enrichment #104

---
+++ data-integration/test/countryEnrichmentTestHelper.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import utils
+
+class CountryEnrichmentTestHelper:
+
+  # ---------------------------------------------------------------------------
+  def __init__(self, data):
+    self.df = pd.DataFrame(data)
+
+  # ---------------------------------------------------------------------------
+  def kbrIdentifierHasCountry(self, identifier, country):
+    """
+    >>> data = CountryEnrichmentTestHelper([{"targetKBRIdentifier": "1", "targetCountryOfPublication": "Belgium"},{"targetKBRIdentifier": "2", "targetCountryOfPublication": "Germany"}, {"targetKBRIdentifier": "3", "targetCountryOfPublication": ""}])
+    >>> data.kbrIdentifierHasCountry("1", "Belgium")
+    True
+    >>> data.kbrIdentifierHasCountry("2", "Germany")
+    True
+    >>> data.kbrIdentifierHasCountry("1", "France")
+    False
+    >>> data.kbrIdentifierHasCountry("3", "")
+    True
+    """
+    foundCountry = utils.getDfCellValue(self.df, "targetKBRIdentifier", identifier, "targetCountryOfPublication")
+
+  # ---------------------------------------------------------------------------
+  def getKBRIdentifierCountry(self, identifier):
+    return utils.getDfCellValue(self.df, "targetKBRIdentifier", identifier, "targetCountryOfPublication")
---
+++ data-integration/test/testCountryEnrichment.py
@@ -0,0 +1,61 @@
+import unittest
+import tempfile
+import csv
+import os
+import utils
+import subprocess
+from countryEnrichmentTestHelper import CountryEnrichmentTestHelper
+
+# Don't show the traceback of an AssertionError, because the AssertionError already says what the issue is!
+__unittest = True
+
+# -----------------------------------------------------------------------------
+class TestCountryEnrichment(unittest.TestCase):
+
+  # ---------------------------------------------------------------------------
+  @classmethod
+  def setUpClass(cls):
+    cls.tempEnriched = os.path.join(tempfile.gettempdir(), 'country-enriched.csv')
+
+    # Run the script whose output we want to test
+    #
+    venv = 'py-integration-env/bin/python'
+    args = [venv, 
+      'add_country.py',
+      '-g', 'test/resources/country-enrichment/geonames/',
+      '-i', 'test/resources/country-enrichment/testdata.csv',
+      '-c', 'targetCountryOfPublication',
+      '-p', 'targetPlaceOfPublication',
+      '-o', cls.tempEnriched]
+    subprocess.run(args)
+
+    # read the script output into an internal data structure
+    #
+    with open(cls.tempEnriched, 'r') as fileIn:
+      csvReader = csv.DictReader(fileIn, delimiter=',')
+      csvData = [dict(d) for d in csvReader]
+      cls.data = CountryEnrichmentTestHelper(csvData)
+
+  # ---------------------------------------------------------------------------
+  def testEmptyLocationEmptyCountry(self):
+    """When the location column is empty, the country column should be empty too"""
+    foundCountry = TestCountryEnrichment.data.getKBRIdentifierCountry("2")
+    self.assertEqual(foundCountry, "", msg= f'For an empty location the country {foundCountry} was found instead of left empty')
+    
+  # ---------------------------------------------------------------------------
+  def testBelgiumFoundForGhent(self):
+    """When the location is Ghent the country should be Belgium"""
+    foundCountry = TestCountryEnrichment.data.getKBRIdentifierCountry("1")
+    self.assertEqual(foundCountry, "Belgium", msg= f'For the location Ghent the country was {foundCountry} instead of Belgium')
+
+  # ---------------------------------------------------------------------------
+  def testBelgiumFoundForBruxeles(self):
+    """When the location is Bruxeles the country should be Belgium"""
+    foundCountry = TestCountryEnrichment.data.getKBRdentifierCountry("4")
+    self.assertEqual(foundCountry, "Belgium", msg= f'For the location Bruxeles the country was {foundCountry} instead of Belgium')
+
+  # ---------------------------------------------------------------------------
+  def testBelgiumFoundForBruxeles(self):
+    """When the location is Brussels the country should be Belgium"""
+    foundCountry = TestCountryEnrichment.data.getKBRIdentifierCountry("4")
+    self.assertEqual(foundCountry, "Belgium", msg= f'For the location Brussels the country was {foundCountry} instead of Belgium')
