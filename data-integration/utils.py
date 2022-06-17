7a9fec28b3ee9a80f05bc9d2fb65c1dd2fdfa683
Sven Lieber <Sven.Lieber@ugent.be>
2022-06-17 18:09:41 +0200

    new statistics and visualized the trend to fix #120

--- data-integration/utils.py
+++ data-integration/utils.py
@@ -6,6 +6,7 @@ import re
 import os
 import unicodedata as ud
 import pandas as pd
+import matplotlib.pyplot as plt
 from SPARQLWrapper import SPARQLWrapper, TURTLE, JSON, POST
 
 # -----------------------------------------------------------------------------
@@ -786,6 +787,58 @@ def countColumnOccurrence(df, columnName, value):
   2
   """
   return df[df[columnName].str.contains(value)].shape[0]
+
+# -----------------------------------------------------------------------------
+def mergeMeasurementsToDataFrame(folder, files):
+  """This function reads the content of the given CSV files and merges them into a Pandas dataframe."""
+
+  filePaths = [os.path.join(f) for f in files]
+  df = pd.concat(map(pd.read_csv, filePaths), ignore_index=True)
+  df['date'] = pd.to_datetime(df['date'])
+  df['date'] = df['date'].dt.date
+  return df.set_index('date')
+
+# -----------------------------------------------------------------------------
+def plotTranslationStats(df, statsName):
+  """This function plots different graphs related to statistics of the corpus."""
+  fig, axs = plt.subplots(2, 2, figsize=(10,5))
+  selectionSources = ['numberTranslations', 'withKBRIdentifier', 'withBnFIdentifier', 'withKBIdentifier']
+  selectionTargetISBN = ['numberTranslations', 'withTargetISBN10', 'withTargetISBN13']
+  selectionSourceInfo = ['numberTranslations', 'withKBRSourceTitle', 'withKBSourceTitle', 'withSourceISBN10', 'withSourceISBN13']
+
+  df[selectionSources].plot(ax=axs[0, 0], kind='line', alpha=0.75, rot=25, legend=None)
+  df[selectionTargetISBN].plot(ax=axs[0,1], kind='line', alpha=0.75, rot=25, legend=None)
+  df[selectionSourceInfo].plot(ax=axs[1,0], kind='line', alpha=0.75, rot=25, legend=None)
+
+  #plt.subplots_adjust(bottom=0.5)
+  fig.suptitle(statsName)
+  fig.tight_layout()
+  fig.legend(loc='center left', bbox_to_anchor=(1.0, 0.5), prop={'size': 10})
+
+# -----------------------------------------------------------------------------
+def plotTranslationStatsIdentifiers(df, statsName):
+  """This function plots different graphs related to statistics of the corpus."""
+
+  selectionSources = ['numberTranslations', 'withKBRIdentifier', 'withBnFIdentifier', 'withKBIdentifier']
+  df[selectionSources].plot(title=statsName, kind='line', alpha=0.75, rot=25).legend(loc='center left', bbox_to_anchor=(1.0, 0.5), prop={'size': 10})
+
+
+
+# -----------------------------------------------------------------------------
+def plotTranslationStatsISBN(df, statsName):
+  """This function plots different graphs related to statistics of the corpus."""
+
+  selectionTargetISBN = ['numberTranslations', 'withTargetISBN10', 'withTargetISBN13']
+  df[selectionTargetISBN].plot(title=statsName, kind='line', alpha=0.75, rot=25).legend(loc='center left', bbox_to_anchor=(1.0, 0.5), prop={'size': 10})
+
+# -----------------------------------------------------------------------------
+def plotTranslationStatsSources(df, statsName):
+  """This function plots different graphs related to statistics of the corpus."""
+
+  selectionSourceInfo = ['numberTranslations', 'withKBRSourceTitle', 'withKBSourceTitle', 'withSourceISBN10',
+                         'withSourceISBN13']
+
+  df[selectionSourceInfo].plot(title=statsName, kind='line', alpha=0.75, rot=25).legend(loc='center left', bbox_to_anchor=(1.0, 0.5), prop={'size': 10})
 
 # -----------------------------------------------------------------------------
 if __name__ == "__main__":
