3b5c40b533a3b49547f43ca6335a69caab28d531
Sven Lieber <Sven.Lieber@kbr.be>
2022-07-26 14:45:38 +0200

    let users also extract skos properties

--- data-sources/bnf/get-subjects.py
+++ data-sources/bnf/get-subjects.py
@@ -12,6 +12,7 @@ import utils
 from predicate_object_filter import PredicateObjectConfigFilter
 from predicate_object_filter import PredicateObjectLookupFilter
 
+
 NS_RDF = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
 NS_RDFS = "http://www.w3.org/2000/01/rdf-schema#"
 NS_SCHEMA = "http://schema.org/"
@@ -22,11 +23,12 @@ NS_OWL = "http://www.w3.org/2002/07/owl#"
 NS_XSD = "http://www.w3.org/2001/XMLSchema#"
 NS_FOAF = "http://xmlns.com/foaf/0.1/"
 NS_VOID = "http://rdfs.org/ns/void#"
+NS_SKOS = "http://www.w3.org/2004/02/skos/core#"
 NS_RDAGROUP1 = "http://rdvocab.info/Elements/"
 NS_RDAGROUP2 = "http://rdvocab.info/ElementsGr2/"
 NS_MARCREL = "http://id.loc.gov/vocabulary/relators/"
 
-ALL_NS = {'rdf': NS_RDF, 'rdfs': NS_RDFS, 'schema': NS_SCHEMA, 'madsrdf': NS_MADSRDF, 'dcterms': NS_DCTERMS, 'isni': NS_ISNI, 'owl': NS_OWL, 'xsd': NS_XSD, 'foaf': NS_FOAF, 'void': NS_VOID, 'rdagroup1elements': NS_RDAGROUP1, 'rdagroup2elements': NS_RDAGROUP2, 'marcrel': NS_MARCREL}
+ALL_NS = {'rdf': NS_RDF, 'rdfs': NS_RDFS, 'schema': NS_SCHEMA, 'madsrdf': NS_MADSRDF, 'dcterms': NS_DCTERMS, 'isni': NS_ISNI, 'owl': NS_OWL, 'xsd': NS_XSD, 'foaf': NS_FOAF, 'void': NS_VOID, 'rdagroup1elements': NS_RDAGROUP1, 'rdagroup2elements': NS_RDAGROUP2, 'marcrel': NS_MARCREL, 'skos': NS_SKOS}
 
 
 RDF_ABOUT = '{http://www.w3.org/1999/02/22-rdf-syntax-ns#}about'
