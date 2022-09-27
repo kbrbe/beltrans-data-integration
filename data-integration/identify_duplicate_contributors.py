b7bdc5cf67d9e1383d5e03428b8d7b4ee3cbe26c
Sven Lieber <Sven.Lieber@ugent.be>
2022-09-27 11:38:47 +0200

    configurable which columns should be added to the output

--- data-integration/identify_duplicate_contributors.py
+++ data-integration/identify_duplicate_contributors.py
@@ -3,9 +3,7 @@
 # KBR Brussels
 #
 import csv
-import utils
 from optparse import OptionParser
-
 import utils_string
 
 
@@ -14,7 +12,7 @@ def checkArguments():
   parser = OptionParser(usage="usage: %prog [options]")
   parser.add_option('-i', '--input', action='store', help='Input CSV file')
   parser.add_option('-o', '--output', action='store', help='Second CSV file')
-  parser.add_option('--id-column', action='store', help='The column containing the row identifier')
+  parser.add_option('--output-column', action='append', help='The name of a column which should be included in the output')
   parser.add_option('--column', action='store',
                     help='column name of the CSV in which it is checked for missing contributor overlap')
   parser.add_option('--csv-delimiter', action='store', default=',', help='Delimiter of the input CSV, default is a comma')
@@ -22,29 +20,33 @@ def checkArguments():
 
   (options, args) = parser.parse_args()
 
-  if (not options.input) or (not options.output) or (not options.column) or (not options.id_column):
+  if (not options.input) or (not options.output) or (not options.column) or (not options.output_column):
     print(f'Input and output data as well as a column name are needed')
     parser.print_help()
     exit(1)
 
   return options, args
-
+   
 # -----------------------------------------------------------------------------
-def addIdentifier(identifier, identifierList):
-  if identifier in identifierList:
-    print(f'Warning, identifier {identifier} was already found, column values are not unique!')
-  else:
-    identifierList.add(identifier)
-    
-# -----------------------------------------------------------------------------
-def main(inputFile, outputFile, idColumn, columnName, csvDelimiter, columnValueDelimiter):
-  """This script reads a CSV files and checks if values in a given column occur more than once in the column."""
+def main(inputFile, outputFile, outputColumns, columnName, csvDelimiter, columnValueDelimiter):
+  """This script reads a CSV files and checks if parts of the value in a given column occur more than once in the column.
+     For example "Sven Lieber (abc, def); Lieber, Sven (abc)" where the first name but also the last name occur twice."""
 
   with open(inputFile, 'r') as inFile, \
        open(outputFile, 'w') as outFile:
 
     inputReader = csv.DictReader(inFile, delimiter=csvDelimiter)
-    outputWriter = csv.DictWriter(outFile, fieldnames=[idColumn, columnName], delimiter=csvDelimiter)
+
+    inputColumns = set(inputReader.fieldnames)
+    wantedColumns = set(outputColumns + [columnName])
+    nonExistentColumns = wantedColumns.difference(inputColumns)
+    if len(nonExistentColumns) > 0:
+      text = 'columns are' if len(nonExistentColumns) > 1 else 'column is'
+      print(f'The following requested {text} not in the input: {nonExistentColumns}')
+      exit(1)
+
+    # not using list(wantedColumns) as fieldnames because we want to determine the order
+    outputWriter = csv.DictWriter(outFile, fieldnames=outputColumns + [columnName], delimiter=csvDelimiter)
 
     identifiers = set()
     numberRows = 0
@@ -52,21 +54,25 @@ def main(inputFile, outputFile, idColumn, columnName, csvDelimiter, columnValueD
     outputWriter.writeheader()
     for row in inputReader:
       columnValue = row[columnName]
+      # we may have "name1 (id1, idn) ; name2 (id2, idn)" and split in this example by ';' to treat each name-ids combination seperately
       values = columnValue.split(columnValueDelimiter)
 
       if len(values) > 1:
         valueNameParts = []
         for value in values:
+          # the input format is "name (id1, idn)", and we want the name
           valueNamePart = value.split('(')[0]
           valueNameParts.append(valueNamePart)
         if utils_string.overlappingValues(valueNameParts):
           numberFound += 1
-          outputWriter.writerow({idColumn: row[idColumn], columnName: row[columnName]})
+          # adding all wanted columns to the output
+          outputRow = {key: row[key] for key in wantedColumns}
+          outputWriter.writerow(outputRow)
       numberRows += 1
 
     print(f'{numberFound}/{numberRows} contain possible contributor duplicates.')
 
 if __name__ == '__main__':
   (options, args) = checkArguments()
-  main(options.input, options.output, options.id_column, options.column, options.csv_delimiter, options.column_value_delimiter)
+  main(options.input, options.output, options.output_column, options.column, options.csv_delimiter, options.column_value_delimiter)
 
