33d72ac5c741182667e44ab757cdd556ad5d4616
Sven Lieber <Sven.Lieber@ugent.be>
2022-12-14 14:43:43 +0100

    made the activation script more generic

--- activate.sh
+++ activate.sh
@@ -2,5 +2,4 @@
 . data-integration/py-integration-env/bin/activate
 
 # make local Python modules available to be used via python -m tools.*
-export PYTHONPATH=/home/slieber/repos/beltrans-data
-
+export PYTHONPATH=$(pwd)
