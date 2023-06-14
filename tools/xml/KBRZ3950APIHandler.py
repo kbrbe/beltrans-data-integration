c26a7f743aa634495ce2c6d2bd67cb9285b59adb
Sven Lieber <Sven.Lieber@ugent.be>
2023-06-14 10:44:06 +0200

    adapted Z3950 API client #187

--- tools/xml/KBRZ3950APIHandler.py
+++ tools/xml/KBRZ3950APIHandler.py
@@ -1,5 +1,6 @@
 import requests
 import urllib.parse
+import time
 import lxml.etree as ET
 
 class KBRZ3950APIHandler:
@@ -12,24 +13,31 @@ class KBRZ3950APIHandler:
         self._queryURL = None
         self._batchSize = 200
 
+    def setBatchSize(self, batchSize):
+        self._batchSize = int(batchSize)
+
     def query(self, queryString):
         self._queryString = queryString
         self._queryURL = self._baseURL + '&q=' + urllib.parse.quote(queryString)
 
         # query 0 records, because we are only interested in the header mentioning the total number of records
-        response = requests.get(self._queryURL + '&rows=0')
+        numberURL = self._queryURL + '&rows=0'
+
+        response = requests.get(numberURL)
         tree = ET.fromstring(response.content)
-        self._numberResults = tree.find('./result').get('numFound')
+        self._numberResults = int(tree.find('./result').get('numFound'))
 
     def numberResults(self):
         return self._numberResults
 
     def data(self):
-        """This iterator function returns the search results in batches of 200"""
+        """This iterator function returns the search results in batches of specified size"""
         startRecord = 0
-        maxRecord = self._numberResults - 1
+        maxRecord = self._numberResults
 
         for i in range(startRecord, maxRecord, self._batchSize):
-            response = requests.get(self._queryURL + '&rows=' + self._batchSize + '&start=' + i)
+            response = requests.get(self._queryURL + '&rows=' + str(self._batchSize) + '&start=' + str(i))
             tree = ET.fromstring(response.content)
-            yield tree.find('./result/record')
+            records = tree.findall('./result/record')
+            for record in records:
+              yield record
