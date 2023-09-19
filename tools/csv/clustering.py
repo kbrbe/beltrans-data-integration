860c5cd2e54415ae5063fe5dc8b21bcdcc641525
Sven Lieber <Sven.Lieber@ugent.be>
2023-09-19 17:22:46 +0200

    added experimental parallelization of distance matrix-based clustering

--- tools/csv/clustering.py
+++ tools/csv/clustering.py
@@ -7,12 +7,13 @@ import csv
 import argparse
 import numpy as np
 from sklearn.cluster import AgglomerativeClustering
-from concurrent.futures import ThreadPoolExecutor
+from scipy import sparse
+from concurrent.futures import ProcessPoolExecutor
 from tools import utils
 import time
 
 # -----------------------------------------------------------------------------
-def main(inputFilename, outputFilename, idColumnName, keyColumnName, delimiter):
+def main(inputFilename, outputFilename, idColumnName, keyColumnName, delimiter, chunkSize):
   """This script performs the hierarchical clustering algorithm sklearn.cluster.AgglomerativeClustering
      on the input based on common descriptive keys."""
 
@@ -30,7 +31,7 @@ def main(inputFilename, outputFilename, idColumnName, keyColumnName, delimiter):
     elementIDs = readElements(inputReader, elementIDs, descriptiveKeys, idColumnName, keyColumnName)
 
     # compute a N:N distance matrix
-    distanceMatrix = computeDistanceMatrixParallel(elementIDs, descriptiveKeys)
+    distanceMatrix = computeDistanceMatrixParallel(elementIDs, descriptiveKeys, chunkSize)
 
     # Perform hierarchical clustering
     clusterLabels = performClustering(distanceMatrix)
@@ -81,37 +82,75 @@ def computeDistanceMatrix(elementIDs, descriptiveKeys):
   return distanceMatrix
 
 # -----------------------------------------------------------------------------
-def computeDistanceMatrixParallel(elementIDs, descriptiveKeys):
+def computeDistanceMatrixParallel(elementIDs, descriptiveKeys, chunk_size):
+
+    numWorkers = 20
+    print(f'Parallel distance matrix computation in chunks of {chunk_size} with {numWorkers} workers')
     start_time = time.time()
+
     # Create a distance matrix initialized with zeros
     num_elements = len(elementIDs)
-    distanceMatrix = np.zeros((num_elements, num_elements))
+    #distance_matrix = np.zeros((num_elements, num_elements), dtype=np.float16)
+    distance_matrix = sparse.csr_matrix((num_elements, num_elements), dtype=np.float32)
 
     # Use ThreadPoolExecutor for parallel processing
-    with ThreadPoolExecutor(max_workers=4) as executor:
-        # Compute distances in parallel
-        futures = []
-        for i in range(num_elements):
-            for j in range(i, num_elements):
-                element1 = elementIDs[i]
-                element2 = elementIDs[j]
-                common_keys = -len(descriptiveKeys[element1].intersection(descriptiveKeys[element2]))
-                futures.append(common_keys)
+    with ProcessPoolExecutor(max_workers=numWorkers) as executor:
+        # Compute distances in parallel for chunks of data
+        for start_idx in range(0, num_elements, chunk_size):
+            chunk_results = []
+            chunk_elements = elementIDs[start_idx:start_idx + chunk_size]
+            future = executor.submit(compute_distance_chunk, chunk_elements, elementIDs, descriptiveKeys)
+            chunk_results.append(future)
 
-        # Fill in the distance matrix using the results
-        idx = 0
-        for i in range(num_elements):
-            for j in range(i, num_elements):
-                distanceMatrix[i, j] = futures[idx]
-                distanceMatrix[j, i] = distanceMatrix[i, j]  # Distance matrix is symmetric
-                idx += 1
+           # Fill in the distance matrix using the results for this chunk
+            for chunk_idx, result in enumerate(chunk_results):
+                distances = result.result()
+                for i, distance_row in enumerate(distances):
+                    row_idx = start_idx + i
+                    distance_matrix[row_idx] = distance_row
 
     end_time = time.time()
     diffTime = time.strftime('%H:%M:%S', time.gmtime(end_time - start_time))
     print(f'distanceMatrix computed in {diffTime}')
 
-    return distanceMatrix
+    return distance_matrix
 
+
+
+# -----------------------------------------------------------------------------
+def compute_distance_chunk(chunk_elements, elementIDs, descriptiveKeys):
+    """
+    Compute distances between elements in a chunk and all other element IDs using descriptive keys.
+
+    Args:
+        chunk_elements (list): List of elements in the chunk.
+        elementIDs (list): List of all element IDs.
+        descriptiveKeys (dict): Dictionary mapping element IDs to their descriptive keys.
+
+    Returns:
+        list: A 2D list where each element represents the negative number of common keys between two elements.
+
+    Example:
+    >>> elementIDs = ['A', 'B', 'C', 'D']
+    >>> descriptiveKeys = {
+    ...     'A': {'key1', 'key2'},
+    ...     'B': {'key2', 'key3'},
+    ...     'C': {'key4'},
+    ...     'D': {'key3'}
+    ... }
+    >>> chunk_elements = ['A', 'B']
+    >>> compute_distance_chunk(chunk_elements, elementIDs, descriptiveKeys)
+    [[0, -1, 0, -1], [-1, 0, -1, 0]]
+
+    """
+    distances = []
+    for element1 in chunk_elements:
+        distance_row = []
+        for element2 in elementIDs:
+            common_keys = descriptiveKeys[element1].intersection(descriptiveKeys[element2])
+            distance_row.append(-len(common_keys))
+        distances.append(distance_row)
+    return distances
 
 
 # -----------------------------------------------------------------------------
@@ -139,6 +178,7 @@ def parseArguments():
   parser.add_argument('--id-column', action='store', required=True, help='The name of the column with element identifiers')
   parser.add_argument('--key-column', action='store', required=True, help="The name of the column that contains a descriptive key")
   parser.add_argument('--delimiter', action='store', default=',', help="Optional delimiter of the input/output CSV, default is ','")
+  parser.add_argument('--chunk-size', action='store', default=500, type=int, help="Optional chunk size for parallelization of distance matrix calculation")
   options = parser.parse_args()
 
   return options
@@ -146,5 +186,5 @@ def parseArguments():
 # -----------------------------------------------------------------------------
 if __name__ == '__main__':
   (options) = parseArguments()
-  main(options.input_file, options.output_file, options.id_column, options.key_column, options.delimiter)
+  main(options.input_file, options.output_file, options.id_column, options.key_column, options.delimiter, options.chunk_size)
  
