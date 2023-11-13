7b0ad6186ce0eded4640d6a63bb60765c68669e7
Sven Lieber <Sven.Lieber@ugent.be>
2023-11-13 09:55:36 +0100

    added script to quickly recreate BELTRANS originals information

---
+++ data-integration/2023-11-08_recreate-originals-graph.sh
@@ -0,0 +1,9 @@
+export $(cat .env | sed 's/#.*//g' | xargs)
+
+sparqlEndpoint=$ENV_SPARQL_ENDPOINT_INTEGRATION
+
+echo "Start queries against '$sparqlEndpoint'"
+python -m tools.sparql.delete_named_graph -u $sparqlEndpoint -n "http://beltrans-originals"
+python -m tools.sparql.upload_data -u $sparqlEndpoint --content-type "application/sparql-update" sparql-queries/create-beltrans-originals.sparql
+python -m tools.sparql.upload_data -u $sparqlEndpoint --content-type "application/sparql-update" integration_queries/link-beltrans-original-manifestations-contributors.sparql
+python -m tools.sparql.upload_data -u $sparqlEndpoint --content-type "application/sparql-update" sparql-queries/create-bibframe-titles.sparql
