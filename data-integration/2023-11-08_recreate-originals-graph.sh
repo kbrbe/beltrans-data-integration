a59064af2159be462b52217ccd086b76dec7881f
Sven Lieber <Sven.Lieber@ugent.be>
2023-11-14 16:31:21 +0100

    verify that source titles are fetched correctly

--- data-integration/2023-11-08_recreate-originals-graph.sh
+++ data-integration/2023-11-08_recreate-originals-graph.sh
@@ -4,6 +4,7 @@ sparqlEndpoint=$ENV_SPARQL_ENDPOINT_INTEGRATION
 
 echo "Start queries against '$sparqlEndpoint'"
 python -m tools.sparql.delete_named_graph -u $sparqlEndpoint -n "http://beltrans-originals"
+python -m tools.sparql.upload_data -u $sparqlEndpoint --content-type "application/sparql-update" sparql-queries/derive-single-title-from-bibframe-titles.sparql
 python -m tools.sparql.upload_data -u $sparqlEndpoint --content-type "application/sparql-update" sparql-queries/create-beltrans-originals.sparql
 python -m tools.sparql.upload_data -u $sparqlEndpoint --content-type "application/sparql-update" integration_queries/link-beltrans-original-manifestations-contributors.sparql
 python -m tools.sparql.upload_data -u $sparqlEndpoint --content-type "application/sparql-update" sparql-queries/create-bibframe-titles.sparql
